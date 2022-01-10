69b5a65c2a67a2875b941ec75e35bcec083d377b
startas <83121954+startdata@users.noreply.github.com>
2022-01-11 03:15:02 +0900

    68

---
+++ src/Java200/StringTest.java
@@ -0,0 +1,29 @@
+package Java200;
+
+public class StringTest {
+    public static void main(String[] args) {
+        String text="Hello Java4Android";
+        String com="HEllo Java4Android";
+
+        System.out.println(text.charAt(2));
+        System.out.println(text.concat("s"));
+        System.out.println(text.contains("And"));
+        System.out.println(text.equals(com));
+        System.out.println(text.equalsIgnoreCase(com));
+        System.out.println(text.indexOf("a")); //'a'
+        System.out.println(text.lastIndexOf("a")); //'a'
+        System.out.println(text.trim());
+        System.out.println(text.length());
+        System.out.println(text.substring(7));
+        System.out.println(text.substring(7,11));
+        System.out.println(text.replace("","-")); //replaceAll
+        System.out.println(text.replace("","-"));
+        System.out.println(text.toUpperCase());
+        System.out.println(text.toLowerCase());
+
+        String[] sp = text.split("");
+        for(int i =0; i<sp.length; i++){
+            System.out.println(i+"\t\t"+sp[i]+"\t\t"+sp[i].length());
+        }
+    }
+}
