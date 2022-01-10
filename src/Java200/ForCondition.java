2a1d912ae38c7aa11bc97e2525fb06d589841a20
startas <83121954+startdata@users.noreply.github.com>
2022-01-10 22:35:03 +0900

    55

---
+++ src/Java200/ForCondition.java
@@ -0,0 +1,18 @@
+package Java200;
+// s=s+a
+public class ForCondition {
+    public static void main(String[] args) {
+        int s = 0;
+        for(int i = 1;i<= 100; i++){
+            s+=i;
+        }
+        System.out.println("1~100의 합:"+s);
+        s = 0; //0으로 초기화
+        for(int i = 1; i <= 100; i++){
+            if(i%2==1){
+                s+=i;
+            }
+        }
+        System.out.println("1~100 사이의 홀수의합:" +s);
+    }
+}
---
+++ src/Java200/ForeachLotto.java
@@ -0,0 +1,11 @@
+package Java200;
+// 향상된 for와 배열
+public class ForeachLotto {
+    public static void main(String[] args) {
+        int[] mm = {1,6,16,22,23,33};
+        for(int m : mm){
+            System.out.printf(m+"\t");
+        }
+        System.out.println();
+    }
+}
