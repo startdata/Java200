f3a4a68bcca3c647b75d39371a1507d0af8b78ba
startas <83121954+startdata@users.noreply.github.com>
2022-01-12 05:04:44 +0900

    70

---
+++ src/Java200/StringBuilderMain70.java
@@ -0,0 +1,28 @@
+package Java200;
+
+public class StringBuilderMain70 {
+    public static void main(String[] args) {
+    //synchronized 안한 StringBuffer
+        StringBuilder sbu = new StringBuilder();
+        sbu.append("I")
+                .append("go")
+                .append("to school.");
+        System.out.println(sbu);
+        sbu.replace(7,11,"");
+        System.out.println(sbu);
+        sbu.reverse();
+        System.out.println(sbu);
+        sbu.deleteCharAt(3);
+        System.out.println(sbu);
+        sbu.delete(1,3);
+        System.out.println(sbu);
+        String ss = sbu.substring(0);
+        System.out.println(ss);
+        System.out.println(sbu);
+        String st = sbu.substring(0,4);
+        System.out.println(st);
+        System.out.println(sbu);
+    }
+}
+
+
