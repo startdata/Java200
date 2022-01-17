a7ddecb64fc5c98972ff94b04a9c743b2f8b8ff3
startas <83121954+startdata@users.noreply.github.com>
2022-01-17 23:53:27 +0900

    78

---
+++ src/Java200/StringArray.java
@@ -0,0 +1,15 @@
+package Java200;
+
+public class StringArray {
+    public static void main(String[] args) {
+       String[] myNum = new String[]{"2016-08-17","2016-03-17"};
+       String[] myNum2 = {"2016-08-17","2016-09-17","2016-03-17"};
+        System.out.println(myNum2[0]);
+        print(myNum);
+        
+        myNum = new String[]{"2016-08-17","2016-03-17"};
+    }
+
+    private static void print(String[] myNum) {
+    }
+}
