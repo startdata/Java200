948c4b9c53395cdb2c988daeca97e45c4fd71f04
startas <83121954+startdata@users.noreply.github.com>
2022-01-24 00:15:18 +0900

    88

---
+++ src/Java200/BillFiles.java
@@ -0,0 +1,33 @@
+package Java200;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.Date;
+
+//파일
+public class BillFiles {
+    public static void main(String[] args) {
+        File f = new File("billboard"); //billboard 디렉토리
+        File[] fd = f.listFiles(); //바로 아래 파일들(지식 파일)
+        for(File ff: fd){
+            String fname = ff.getName(); //파일명
+            String post = fname.substring(fname.lastIndexOf(".")+1);
+
+            System.out.println(fname+" "+post); //이름, 확장자
+            System.out.println(ff.getAbsolutePath());   //전체 경로
+            System.out.println(new Date(ff.lastModified())); //수정일
+            try(BufferedReader br = new BufferedReader(
+                    new FileReader(ff.getAbsolutePath())
+            )){
+                String msg = "";
+                while((msg=br.readLine())!=null){
+                    System.out.println(msg);
+                }
+            } catch (Exception e){
+                System.out.println(e);
+            }
+            System.out.println("=======================");
+        }
+    }
+}
