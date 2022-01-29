04965012e5002c9940d0d7a7da9e9f0da02e61ab
startas <83121954+startdata@users.noreply.github.com>
2022-01-29 16:39:56 +0900

    93

---
+++ src/Java200/RequestFromBillboardHot93.java
@@ -0,0 +1,16 @@
+package Java200;
+
+import java.util.ArrayList;
+
+public class RequestFromBillboardHot93 {
+    ArrayList<String> htmls = new ArrayList<String>();
+    ArrayList<Billboard> billboard = new ArrayList<Billboard>();
+    boolean isConnection = false;
+    public void RequestFromBillboardHot2(){
+        htmls.clear();
+        billboard.clear();
+    }
+    public ArrayList<Billboard> getBillboard(){
+        return billboard;
+    }
+}
