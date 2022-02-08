05ce8b241656ca0b438d9ecf640c499b07fa660f
startas <83121954+startdata@users.noreply.github.com>
2022-02-08 20:11:16 +0900

    106

---
+++ src/Java200/CardCaseMain.java
@@ -0,0 +1,15 @@
+package Java200;
+
+public class CardCaseMain {
+    public static void main(String[] args) {
+        CardCase cc = new CardCase();
+        cc.make(); //서로 다른 카드 20장 만들기
+        cc.print();
+        System.out.println("=================");
+        cc.shuffle(); //카드섞기
+        cc.print();
+        System.out.println("=================");
+        cc.shuffle(); //카드섞기
+        cc.print();
+    }
+}
